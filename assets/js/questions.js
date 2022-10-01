c4532967157597d5947070591cae5cdc0c067e32
JAG <jagrasso42@gmail.com>
2022-10-01 15:05:42 -0400

    added event listener for Start button, function startGame() and function buildQuestion() to script.js, added questions objects to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+  {
+    text: "question 1",
+    choices: ["a", "b", "c", "d"],
+    correct: "a"
+  },
+  {
+    text: "question 2",
+    choices: ["a", "b", "c", "d"],
+    correct: "b"
+  },
+  {
+    text: "question 3",
+    choices: ["a", "b", "c", "d"],
+    correct: "c"
+  }
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 //click start button ask first question
 //select start button
 
+var buttonEl = document.getElementById("start-button");
+
+function startGame() {
+  document.querySelector(".start").classList.add("hide");
+  document.querySelector(".quiz").classList.remove("hide");
+  buildQuestion();
+}
+
+function buildQuestion () {
+  document.getElementById("question-text").textContent = questions[0].text;
+
+  var buttonBox = document.querySelector(".button-box");
+  questions[0].choices.forEach(function(choice) {
+    var button = document.createElement("button");
+    button.textContent = choice;
+    buttonBox.appendChild(button);
+  }) 
+}
+
+buttonEl.addEventListener("click", startGame);
